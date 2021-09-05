b20e233461ba29be2f72f336a2c05f01a1d97490
chunzhang <chun.zhang.cz@gmail.com>
2021-09-05 13:15:45 -0700

    add reference solution
    943

--- leetcode/0943_find_the_shortest_superstring/sol.cpp
+++ leetcode/0943_find_the_shortest_superstring/sol.cpp
@@ -1,27 +1,27 @@
 /*
-Given an array of strings words, return the smallest string that contains each string in words as a substring. If there are multiple valid strings of the smallest length, return any of them.
+  Given an array of strings words, return the smallest string that contains each string in words as a substring. If there are multiple valid strings of the smallest length, return any of them.
 
-You may assume that no string in words is a substring of another string in words.
+  You may assume that no string in words is a substring of another string in words.
 
  
 
-Example 1:
+  Example 1:
 
-Input: words = ["alex","loves","leetcode"]
-Output: "alexlovesleetcode"
-Explanation: All permutations of "alex","loves","leetcode" would also be accepted.
-Example 2:
+  Input: words = ["alex","loves","leetcode"]
+  Output: "alexlovesleetcode"
+  Explanation: All permutations of "alex","loves","leetcode" would also be accepted.
+  Example 2:
 
-Input: words = ["catg","ctaagt","gcta","ttca","atgcatc"]
-Output: "gctaagttcatgcatc"
+  Input: words = ["catg","ctaagt","gcta","ttca","atgcatc"]
+  Output: "gctaagttcatgcatc"
  
 
-Constraints:
+  Constraints:
 
-1 <= words.length <= 12
-1 <= words[i].length <= 20
-words[i] consists of lowercase English letters.
-All the strings of words are unique.
+  1 <= words.length <= 12
+  1 <= words[i].length <= 20
+  words[i] consists of lowercase English letters.
+  All the strings of words are unique.
 */
 
 // DP based on Hamiltonian path
@@ -193,5 +193,161 @@ private:
         }
         
         return ans;
+    }
+};
+
+
+// Brute force DFS+pruning from HuaHua
+class Solution {
+public:
+    string shortestSuperstring(vector<string>& A) {    
+        const int n = A.size();
+        g_ = vector<vector<int>>(n, vector<int>(n));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j) {
+                g_[i][j] = A[j].length();
+                for (int k = 1; k <= min(A[i].length(), A[j].length()); ++k)
+                    if (A[i].substr(A[i].size() - k) == A[j].substr(0, k))            
+                        g_[i][j] = A[j].length() - k;
+            }
+        vector<int> path(n);
+        best_len_ = INT_MAX;
+        dfs(A, 0, 0, 0, path);    
+        string ans = A[best_path_[0]];
+        for (int k = 1; k < best_path_.size(); ++k) {
+            int i = best_path_[k - 1];
+            int j = best_path_[k];
+            ans += A[j].substr(A[j].length() - g_[i][j]);
+        }
+        return ans;
+    }
+private:
+    vector<vector<int>> g_;
+    vector<int> best_path_;
+    int best_len_;
+    void dfs(const vector<string>& A, int d, int used, int cur_len, vector<int>& path) {
+        if (cur_len >= best_len_) return;
+        if (d == A.size()) {
+            best_len_ = cur_len;
+            best_path_ = path;
+            return;
+        }
+    
+        for (int i = 0; i < A.size(); ++i) {
+            if (used & (1 << i)) continue;      
+            path[d] = i;
+            dfs(A,
+                d + 1, 
+                used | (1 << i),
+                d == 0 ? A[i].length() : cur_len + g_[path[d - 1]][i],
+                path);
+        }
+    }
+};
+
+
+// official solution (in Java)
+/*
+We have to put the words into a row, where each word may overlap the previous word. This is because no word is contained in any word.
+
+Also, it is sufficient to try to maximize the total overlap of the words.
+
+Say we have put some words down in our row, ending with word A[i]. Now say we put down word A[j] as the next word, where word j hasn't been put down yet. The overlap increases by overlap(A[i], A[j]).
+
+We can use dynamic programming to leverage this recursion. Let dp(mask, i) be the total overlap after putting some words down (represented by a bitmask mask), for which A[i] was the last word put down. Then, the key recursion is dp(mask ^ (1<<j), j) = max(overlap(A[i], A[j]) + dp(mask, i)), where the jth bit is not set in mask, and i ranges over all bits set in mask.
+
+Of course, this only tells us what the maximum overlap is for each set of words. We also need to remember each choice along the way (ie. the specific i that made dp(mask ^ (1<<j), j) achieve a minimum) so that we can reconstruct the answer.
+
+Algorithm
+
+Our algorithm has 3 main components:
+
+Precompute overlap(A[i], A[j]) for all possible i, j.
+Calculate dp[mask][i], keeping track of the "parent" i for each j as described above.
+Reconstruct the answer using parent information.
+Please see the implementation for more details about each section.
+*/
+class Solution {
+    public String shortestSuperstring(String[] A) {
+        int N = A.length;
+
+        // Populate overlaps
+        int[][] overlaps = new int[N][N];
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j) if (i != j) {
+                int m = Math.min(A[i].length(), A[j].length());
+                for (int k = m; k >= 0; --k)
+                    if (A[i].endsWith(A[j].substring(0, k))) {
+                        overlaps[i][j] = k;
+                        break;
+                    }
+            }
+
+        // dp[mask][i] = most overlap with mask, ending with ith element
+        int[][] dp = new int[1<<N][N];
+        int[][] parent = new int[1<<N][N];
+        for (int mask = 0; mask < (1<<N); ++mask) {
+            Arrays.fill(parent[mask], -1);
+
+            for (int bit = 0; bit < N; ++bit) if (((mask >> bit) & 1) > 0) {
+                // Let's try to find dp[mask][bit].  Previously, we had
+                // a collection of items represented by pmask.
+                int pmask = mask ^ (1 << bit);
+                if (pmask == 0) continue;
+                for (int i = 0; i < N; ++i) if (((pmask >> i) & 1) > 0) {
+                    // For each bit i in pmask, calculate the value
+                    // if we ended with word i, then added word 'bit'.
+                    int val = dp[pmask][i] + overlaps[i][bit];
+                    if (val > dp[mask][bit]) {
+                        dp[mask][bit] = val;
+                        parent[mask][bit] = i;
+                    }
+                }
+            }
+        }
+
+        // # Answer will have length sum(len(A[i]) for i) - max(dp[-1])
+        // Reconstruct answer, first as a sequence 'perm' representing
+        // the indices of each word from left to right.
+
+        int[] perm = new int[N];
+        boolean[] seen = new boolean[N];
+        int t = 0;
+        int mask = (1 << N) - 1;
+
+        // p: the last element of perm (last word written left to right)
+        int p = 0;
+        for (int j = 0; j < N; ++j)
+            if (dp[(1<<N) - 1][j] > dp[(1<<N) - 1][p])
+                p = j;
+
+        // Follow parents down backwards path that retains maximum overlap
+        while (p != -1) {
+            perm[t++] = p;
+            seen[p] = true;
+            int p2 = parent[mask][p];
+            mask ^= 1 << p;
+            p = p2;
+        }
+
+        // Reverse perm
+        for (int i = 0; i < t/2; ++i) {
+            int v = perm[i];
+            perm[i] = perm[t-1-i];
+            perm[t-1-i] = v;
+        }
+
+        // Fill in remaining words not yet added
+        for (int i = 0; i < N; ++i) if (!seen[i])
+            perm[t++] = i;
+
+        // Reconstruct final answer given perm
+        StringBuilder ans = new StringBuilder(A[perm[0]]);
+        for (int i = 1; i < N; ++i) {
+            int overlap = overlaps[perm[i-1]][perm[i]];
+            ans.append(A[perm[i]].substring(overlap));
+        }
+
+        return ans.toString();
     }
 };
