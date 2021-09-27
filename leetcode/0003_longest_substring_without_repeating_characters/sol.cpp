729ad6972fc0ba19e52ece9d4b024b343a930c9b
chunzhang <chun.zhang.cz@gmail.com>
2021-09-27 12:23:00 -0700

    two pointers: sliding window
    3: sliding window

--- leetcode/0003_longest_substring_without_repeating_characters/sol.cpp
+++ leetcode/0003_longest_substring_without_repeating_characters/sol.cpp
@@ -1,29 +1,88 @@
 /*
-  Given a string, find the length of the longest substring without repeating characters.
+  Given a string s, find the length of the longest substring without repeating characters.
 
-  Examples:
+ 
 
-  Given "abcabcbb", the answer is "abc", which the length is 3.
+  Example 1:
 
-  Given "bbbbb", the answer is "b", with the length of 1.
+  Input: s = "abcabcbb"
+  Output: 3
+  Explanation: The answer is "abc", with the length of 3.
+  Example 2:
 
-  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+  Input: s = "bbbbb"
+  Output: 1
+  Explanation: The answer is "b", with the length of 1.
+  Example 3:
+
+  Input: s = "pwwkew"
+  Output: 3
+  Explanation: The answer is "wke", with the length of 3.
+  Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+  Example 4:
+
+  Input: s = ""
+  Output: 0
+ 
+
+  Constraints:
+
+  0 <= s.length <= 5 * 104
+  s consists of English letters, digits, symbols and spaces.
 */
 
+// two pointers
 class Solution {
 public:
-  int lengthOfLongestSubstring(string s) {
-    int maxDist = 0;
-    vector<int> lastIdx(256, -1);  // most recent index when a character appear
-    for(int i=0,j=0; j<s.size(); ++j) {
-      char c = s[j];
-      if(lastIdx[c] != -1) {  // may have repeated characters, so move i when necessary
-	i = max(i, lastIdx[c]+1);  // make sure i doesn't go back
-      }
-      lastIdx[c] = j;
-      maxDist = max(maxDist, j-i+1);
-    }
+    int lengthOfLongestSubstring(string s) {
+        vector<int> counts(256, 0);
+        int ans = 0;
+        for(int inti=0,j=0; j<s.size(); ++j) {  // for substring ends at j
+            counts[s[j]]++;
+            while(counts[s[j]] > 1) {
+                counts[s[i++]]--;
+            }
+            
+            ans = max(ans, j-i+1);
+        }
         
-    return maxDist;
-  }
+        return ans;
+    }
+};
+
+
+// two pointers: another impl
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> counts(256, 0);
+        int ans = 0;
+        for(int i=0,j=0; j<s.size(); ++i) {  // for substring starts at i
+            for(; j<s.size()&&counts[s[j]]==0; ++j)
+                counts[s[j]]++;
+            ans = max(ans, j-i);
+            counts[s[i]]--;
+        }
+        
+        return ans;
+    }
+};
+
+// remember index of char appeared last time
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxDist = 0;
+        vector<int> lastIdx(256, -1);  // most recent index when a character appear
+        for(int i=0,j=0; j<s.size(); ++j) {
+            char c = s[j];
+            if(lastIdx[c] != -1) {  // may have repeated characters, so move i when necessary
+                i = max(i, lastIdx[c]+1);  // make sure i doesn't go back
+            }
+            lastIdx[c] = j;
+            maxDist = max(maxDist, j-i+1);
+        }
+        
+        return maxDist;
+    }
 };
